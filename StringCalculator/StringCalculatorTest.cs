dc2d05dd70a34223dccba2a27723b6084d81b14b
Maria Aldana <maria.aldana@sinco.com.co>
2025-10-22 16:01:10 -0500

    🟥 cadena vacia devuelve cero

---
+++ StringCalculator/StringCalculatorTest.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace StringCalculator;
+
+public class StringCalculator
+{
+    [Fact]
+    public void Si_Envio_Cadena_Vacia_Devuelve_Cero ()
+    {
+        var resultado = Suma("");
+        resultado.Equals(0);
+    }
+
+    private object Suma(string numeros)
+    {
+        throw new NotImplementedException();
+    }
+}
