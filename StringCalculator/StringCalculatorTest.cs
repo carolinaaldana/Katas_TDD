a0340ffd819e013bc4a5d9657b41243d84cc8ca0
Maria Aldana <maria.aldana@sinco.com.co>
2025-10-23 09:12:27 -0500

    🟥 envio número devuelvo número

--- StringCalculator/StringCalculatorTest.cs
+++ StringCalculator/StringCalculatorTest.cs
@@ -9,10 +9,19 @@ public class StringCalculator
         resultado.Equals(0);
     }
 
-    private int Suma(string numeros)
+    [Theory]
+    [InlineData("1")]
+    [InlineData("2")]
+    public void Si_Envio_Un_Numero_Devuelve_El_Mismo_Numero(string numero)
+    {
+        var resultado = Suma(numero);
+        resultado.Equals(numero);
+    }
+
+    private string Suma(string numeros)
     {
         if(numeros == "") 
-            return 0;
-        return 0;
+            return "0";
+        return null;
     }
 }
--- StringCalculator/obj/Debug/net8.0/StringCalculator.AssemblyInfo.cs
+++ StringCalculator/obj/Debug/net8.0/StringCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StringCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20f581aebb174d4fa56ecd59c197ac2b0f5aa7c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+533553503fd4413d54cb19bf711f409b2a779f4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("StringCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StringCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
